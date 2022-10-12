f7a53426b8e8c3f0b269ddd81b1087ff6579eff3
RodrigoGomezUKG <109101927+RodrigoGomezUKG@users.noreply.github.com>
2022-10-11 22:55:00 -0300

    Makes createNewPlayer works with database

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'ukg-ort',
+    appId: '1:657645245751:web:7ab1faf1bb432b8df276b2',
+    storageBucket: 'ukg-ort.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCU4gH3VtnldtOwEkbQUweYJItFQOWmsQU',
+    authDomain: 'ukg-ort.firebaseapp.com',
+    messagingSenderId: '657645245751',
+    measurementId: 'G-2SZZ641T7M',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ukg-ort',
+    appId: '1:657645245751:web:7ab1faf1bb432b8df276b2',
+    storageBucket: 'ukg-ort.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCU4gH3VtnldtOwEkbQUweYJItFQOWmsQU',
+    authDomain: 'ukg-ort.firebaseapp.com',
+    messagingSenderId: '657645245751',
+    measurementId: 'G-2SZZ641T7M',
+  },
   production: false
 };
 
