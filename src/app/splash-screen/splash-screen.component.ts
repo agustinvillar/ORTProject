45860786cd8740cb56b12c536333f81f2e6daeea
ValentinSanchez <62060241+VfSanchezBarre@users.noreply.github.com>
2022-10-14 17:57:27 -0300

    Added splash screen component

---
+++ src/app/splash-screen/splash-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-splash-screen',
+  templateUrl: './splash-screen.component.html',
+  styleUrls: ['./splash-screen.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class SplashScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
