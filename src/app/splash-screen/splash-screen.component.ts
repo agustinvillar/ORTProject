b6ec63737f641824d0fc4e19e0360be2750da55e
ValentinSanchez <62060241+VfSanchezBarre@users.noreply.github.com>
2022-10-14 18:18:06 -0300

    Added animation

--- src/app/splash-screen/splash-screen.component.ts
+++ src/app/splash-screen/splash-screen.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { SplashAnimationType } from './animation-splash';
 
 @Component({
   selector: 'app-splash-screen',
@@ -8,8 +9,40 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class SplashScreenComponent implements OnInit {
 
+  windowWidth: string;
+  splashTransition: string;
+  opacityChange: number = 1;
+  showSplash = true;
+
+  @Input() animationDuration: number = 0.5;
+  @Input() duration: number = 3;
+  @Input() animationType: SplashAnimationType = SplashAnimationType.SlideLeft;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    setTimeout(() => {
+      let transitionStyle = "";
+      switch (this.animationType) {
+        case SplashAnimationType.SlideLeft:
+          this.windowWidth = "-" + window.innerWidth + "px";
+          transitionStyle = "left " + this.animationDuration + "s";
+          break;
+        case SplashAnimationType.SlideRight:
+          this.windowWidth = window.innerWidth + "px";
+          transitionStyle = "left " + this.animationDuration + "s";
+          break;
+        case SplashAnimationType.FadeOut:
+          transitionStyle = "opacity " + this.animationDuration + "s";
+          this.opacityChange = 0;
+      }
+
+      this.splashTransition = transitionStyle;
+
+      setTimeout(() => {
+        this.showSplash = !this.showSplash;
+      }, this.animationDuration * 1000);
+    }, this.duration * 1000);
+  }
 
 }
