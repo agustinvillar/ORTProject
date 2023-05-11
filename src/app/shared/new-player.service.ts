8459d63e34664d30a01c90b9270829b7fa6ffe61
german-ipesriera <german.ipesriera@ukg.com>
2023-05-11 16:22:36 -0300

    added identity card validator

--- src/app/shared/new-player.service.ts
+++ src/app/shared/new-player.service.ts
@@ -2,17 +2,18 @@
 import { Injectable } from '@angular/core';
 import { NewPlayer } from '../shared/new-player';
 import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { getCountFromServer, query, where } from 'firebase/firestore';
 @Injectable({
   providedIn: 'root',
 })
 export class NewPlayerService {
 
   playerName: String
+  playerRef = collection(this.firestore, 'players')
 
   constructor(private firestore: Firestore) { }
   // Create
   createNewPlayer(player: NewPlayer) {
-    const playerRef = collection(this.firestore, 'players')
     const newPlayer = {
       completeName: player.completeName,
       email: player.email,
@@ -24,6 +25,12 @@ export class NewPlayerService {
     }
     this.playerName = player.completeName
     // Add to Firestore
-    return addDoc(playerRef, newPlayer)
+    return addDoc(this.playerRef, newPlayer)
+  }
+
+
+  getAmountOfPlayersByIdentityCard(identityCard: String) {
+    const q = query(this.playerRef, where("identityCard", "==", identityCard))
+    return getCountFromServer(q)
   }
 }
