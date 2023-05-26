7f88abb91e6c70f7c6fee2fb2bab89645f8da549
Darwin <darwin.sosa@ukg.com>
2023-05-26 09:19:59 -0300

    added edad, formacion and sctor fields

--- src/app/shared/new-player.ts
+++ src/app/shared/new-player.ts
@@ -2,5 +2,8 @@ export class NewPlayer {
     completeName: string;
     email: string
     mobile: string;
+    edad: Int32Array;
+    formacion: string;
+    sector: string;
     acceptsConditions : boolean;
 }
